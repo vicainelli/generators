6de0cc78e30fde28519af434d97f96c8c4594f3b
Vinicius Cainelli <vicainelli+github@gmail.com>
2020-07-21 10:28:05 -0300

    Create uuid.ts

---
+++ generators/uuid.ts
@@ -0,0 +1,9 @@
+export const useId = (size = 3) : string => {
+  let uuid:string= "";
+  const dictionary =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < size; i++) {
+    uuid += dictionary.charAt(Math.floor(Math.random() * dictionary.length));
+  }
+  return uuid;
+}
