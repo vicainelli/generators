da6f64bbd9e200d19d8007c5fade3878fffd46e1
Vinicius Cainelli <vicainelli+github@gmail.com>
2020-07-21 10:52:31 -0300

    Create isUndefined.ts

---
+++ validators/isUndefined.ts
@@ -0,0 +1,8 @@
+/**
+ * @description Checks if a value is undefined
+ * @param {*} v
+ * @returns {Boolean}
+ */
+export const isUndef = (v) : Boolean => {
+  return v === undefined || v === null
+}
